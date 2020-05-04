5102bb2915fef9ec15d536897a60b5423c1eb21e
Nicolai Savolyi <code@nsavolyi.xyz>
2020-05-04 10:39:33 +0200

    TinyMCE hinzugefügt

---
+++ resources/js/tinymce.js
@@ -0,0 +1,3 @@
+import tinymce from 'tinymce/tinymce';
+import 'tinymce/themes/silver';
+import 'tinymce/plugins/lists';
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .js('resources/js/tinymce.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .copy('node_modules/tinymce/skins', 'public/css/tinymce/skins');
