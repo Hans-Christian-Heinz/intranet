26f3a5e7584b68557c02b6f02b9e8a48b63020c1
Nicolai Savolyi <code@nsavolyi.xyz>
2020-05-06 09:33:09 +0200

    Weitere Anpassungen für Adminless LDAP

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@
 
 Route::get('/', 'HomeController@index')->name('home');
 
-Auth::routes();
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::group(["middleware" => "auth"], function () {
     // Routen für Berichtsheft:
