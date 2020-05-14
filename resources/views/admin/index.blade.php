add2f4143f04b605faa63accd6ceeca99cdfa65c
Nicolai Savolyi <code@nsavolyi.xyz>
2020-05-14 15:48:48 +0200

    Administration: Dashboard verbessert

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,25 +6,30 @@
     <div class="section">
         <div class="container">
             <div class="row">
-                <div class="col-md-10 offset-md-2 row">
-                    <div class="col-md-3">
-                        <div class="card">
+                <div class="col-md-2"></div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <a href="{{ route('admin.exemptions.index') }}" class="text-dark">
                             <div class="card-body">
-                              <h2 class="card-title text-center">{{ $exemptionCount }}</h2>
-                              <p class="card-text text-center">neue Freistellungsanträge</p>
+                            <h1 class="card-title text-center display-2 {{ $exemptionCount ? 'text-primary font-weight-bold' : '' }}">
+                                {{ $exemptionCount }}
+                            </h1>
+                                <p class="card-text text-center">neue Freistellungsanträge</p>
                             </div>
-                          </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="card">
-                            <div class="card-body">
-                              <h2 class="card-title text-center">{{ $userCount }}</h2>
-                              <p class="card-text text-center mb-4">registrierte Benutzer</p>
-                            </div>
-                          </div>
+                        </a>
                     </div>
                 </div>
 
+                <div class="col-md-4">
+                    <div class="card">
+                        <a href="{{ route('admin.users.index') }}" class="text-dark">
+                            <div class="card-body">
+                                <h1 class="card-title text-center display-2">{{ $userCount }}</h1>
+                                <p class="card-text text-center">registrierte Benutzer</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
